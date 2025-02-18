e9b29355e031bb751c22f5079b236df4bc542b1f
sangwon1233 <tkddnjs3351@naver.com>
2025-02-18 20:12:46 +0900

    product list 수정중

--- front-apd/src/components/ProductList.jsx
+++ front-apd/src/components/ProductList.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 const ProductList = ({ products = [] }) => {  // ✅ 기본값 설정하여 undefined 방지
-    if (products.length === 0) {
-      return <p>상품이 없습니다.</p>;
-    }
-  
-    return (
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-      </ul>
-    );
-  };
-  
+  if (products.length === 0) {
+    return <p>상품이 없습니다.</p>;
+  }
 
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
+    </ul>
+  );
+};
+// useEffect(() => {
+//   axios.get("http://localhost:8080/api/aliexpress/categories")
+//       .then(response => setCategories(response.data))
+//       .catch(error => console.error("Error fetching categories", error));
+// }, []);
 export default ProductList;
